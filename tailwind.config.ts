3aaf3eaf2321a2e17be5b38cb7cfed71c31411f1
Refusado <refusado@gmail.com>
2024-06-10 06:21:42 -0300

    forgot to format these configuration files lol

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
-import { nextui } from "@nextui-org/react";
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/react';
+import type { Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
         cyan: {
-          black: "#060b0e",
+          black: '#060b0e',
         },
       },
     },
@@ -21,39 +21,42 @@ const config: Config = {
       center: true,
       screens: {
         ...defaultTheme.screens,
-        'xl': defaultTheme.screens.lg,
-        '2xl': defaultTheme.screens.lg
-      }
-    }
+        xl: defaultTheme.screens.lg,
+        '2xl': defaultTheme.screens.lg,
+      },
+    },
   },
-  plugins: [nextui({
-    addCommonColors: true,
-    themes: {
-      dark: {
-        colors: {
-          background: {
-            DEFAULT: '#0A1216',
+  plugins: [
+    nextui({
+      addCommonColors: true,
+      themes: {
+        dark: {
+          colors: {
+            background: {
+              DEFAULT: '#0A1216',
+            },
+            overlay: {
+              DEFAULT: '#102A36',
+            },
+            default: {
+              50: '#003C2E',
+              100: '#042E24',
+              200: '#004A39',
+              300: '#105746',
+              400: '#256856',
+              500: '#3B7C69',
+              600: '#5B9C88',
+              700: '#4F8F7C',
+              800: '#87C9B4',
+              900: '#ADEFDA',
+            },
+            primary: '#227D5A',
+            content1: '#111A1F',
           },
-          overlay: {
-            DEFAULT: '#102A36',
-          },
-          default: {
-            50: '#003C2E',
-            100: '#042E24',
-            200: '#004A39',
-            300: '#105746',
-            400: '#256856',
-            500: '#3B7C69',
-            600: '#5B9C88',
-            700: '#4F8F7C',
-            800: '#87C9B4',
-            900: '#ADEFDA',
-          },
-          primary: '#227D5A',
-          content1: '#111A1F',
-        }
-      }
-    }
-  }), require('tailwind-scrollbar')],
+        },
+      },
+    }),
+    require('tailwind-scrollbar'),
+  ],
 };
 export default config;
